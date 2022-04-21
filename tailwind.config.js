e69bc3663d685d01b7ca928320e630efaf7750e6
DarifElmehdi <elmehdi.darif@um5s.net.ma>
2022-04-21 00:50:13 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    content: ["./**/*.{html,js}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                audiowide: ["Audiowide", "cursive"],
+                roboto: ["Roboto", "sans-serif"],
+                popins: ["Poppins", "sans-serif"],
+            },
+            colors: {
+                blue: "#3574C4",
+                orange: "#F44A4A",
+                white: "#FFFFFF",
+            },
+        },
+    },
+    plugins: [],
+};
